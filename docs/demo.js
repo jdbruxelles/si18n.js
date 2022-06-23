43c8654747213ed2c3bf023eac1292218e7516c3
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2022-06-23 02:20:47 +0200

    Delete demo.js

--- docs/demo.js
+++
@@ -1,5 +0,0 @@
-
-  // Show locales infos
-  const infos = document.querySelectorAll("#demo #info span");
-  infos[0].innerText = `${locObj.t("current_lang")} : ${locObj.getLocale()}`;
-  infos[1].innerText = `${locObj.t("all_lang")} : ${locObj.getLocales().join(", ")}`;
