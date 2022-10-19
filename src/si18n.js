d3cbdff3fd677d694dca851b011567625b86344d
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2022-10-19 15:16:15 +0200

    feat: Allow automatic loading of the translations files using the new `path`   and `availableLocales` options.

--- src/si18n.js
+++ src/si18n.js
@@ -15,6 +15,8 @@ export default class Si18n {
     lang: "",
     fallbackLang: "",
     locales: {}, // Required
+    availableLocales: [],
+    path: null,
     activeClass: "",
     togglersSelector: "",
     isTogglerSelect: false,
@@ -49,6 +51,8 @@ export default class Si18n {
    *   3. Hard coded lang,
    *   4. Navigator lang.
    * @param {string} [_options.fallbackLang=_options.lang] Fallback language.
+   * @param {string} [_options.path] Path to the JSON files.
+   * @param {string} [_options.availableLocales] List of available languages.
    * @param {string} [_options.activeClass] Class to add to the active language.
    * @param {string} _options.togglersSelector Selector to the togglers elements.
    * @param {boolean} [_options.isTogglerSelect=false] Whether the toggler is a select or a button.
@@ -62,6 +66,12 @@ export default class Si18n {
 
     const docsLink = "https://si18n.js.bruxelles.dev/#options";
     const optionsKeys = Object.keys(_options);
+    const autoLoad = optionsKeys.includes("path");
+    const throwInvalidSelector = () => {
+      throw new Error("The togglersSelector option must be a valid selector." +
+        " Or you may forget to add the selected HTML element(s). See docs " +
+        docsLink);
+    };
 
     if (optionsKeys.length === 0) {
       throw new Error(`No options provided. See docs ${docsLink}`);
@@ -79,11 +89,13 @@ export default class Si18n {
       else throw new Error(`The fallback option is required when no lang option is set. See docs ${docsLink}`);
     }
 
+    this.#options.availableLocales = _options.availableLocales || this.getLocales();
+
     if (_options.translate) this.#options.translate = _options.translate;
     if (_options.locales) this.#options.locales = _options.locales;
     if (_options.saveAs) this.#options.saveAs = _options.saveAs;
-    if (typeof _options.onChange === "function") {
-      this.#options.callback = _options.onChange;
+    if (typeof _options.onLocaleChanged === "function") {
+      this.#options.callback = _options.onLocaleChanged;
     }
 
     const searchParam = new URLSearchParams(window.location.search);
@@ -92,7 +104,7 @@ export default class Si18n {
       this.#options.saveLang = _options.saveLang;
     }
 
-    if (langInURL !== null && !Si18n.#isUndefined(this.#options.locales[langInURL])) {
+    if (langInURL !== null && this.#options.availableLocales.includes(langInURL)) {
       this.#options.lang = langInURL;
     } else if (this.#options.saveLang) {
       this.#options.lang = localStorage.getItem(this.#options.saveAs) || _options.lang;
@@ -103,8 +115,33 @@ export default class Si18n {
       this.#options.lang = navigator.language.substring(0, 2);
     }
 
-    this.#translate();
-    this.#options.callback();
+    if (autoLoad) {
+      // path option required on autoLoad mode.
+      if (typeof _options.path !== "string") {
+        throw new Error(`The path option must be a string. See docs ${docsLink}`);
+      }
+
+      if ( // availableLocales option required on autoLoad mode.
+        !Array.isArray(_options.availableLocales) ||
+        (Array.isArray(_options.availableLocales) &&
+        _options.availableLocales.length === 0)
+      ) {
+        throw new Error(`The availableLocales option is required when using the path option. See docs ${docsLink}`);
+      }
+
+      this.#options.path = _options.path;
+      if (this.#options.lang !== this.#options.fallbackLang) {
+        this.#loadLocale({ lang: this.#options.fallbackLang });
+      }
+      this.#loadLocale({ lang: this.#options.lang, cb: () => {
+        this.#translate();
+        this.#options.callback();
+      }});
+    } else {
+      this.#translate();
+      this.#options.callback();
+    }
+
     this.#isInitialized = true;
 
     // Configure buttons which will switch languages.
@@ -144,7 +181,7 @@ export default class Si18n {
         }
 
         buttons.forEach((button) => {
-          if (this.getLocales().includes(button.dataset.lang)) {
+          if (this.#options.availableLocales.includes(button.dataset.lang)) {
             button.addEventListener("click", () => {
               if (hasActiveClass) {
                 buttons.forEach((button_) => {
@@ -168,7 +205,12 @@ export default class Si18n {
    * @param {string} lang the language to set.
    */
   setLocale(lang) {
-    this.#translate(lang);
+    this.#loadLocale({ lang, cb: () => {
+      this.#options.lang = lang;
+      this.#translate();
+      this.#options.callback();
+    }});
+
     if (this.#options.saveLang) {
       localStorage.setItem(this.#options.saveAs, lang);
     }
@@ -204,6 +246,16 @@ export default class Si18n {
    * @param {string} url The URL to fetch.
    * @param {function(object=):void} [callback] The callback function to
    * execute when the data is fetched.
+   */
+  static async getJSON(url, callback) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((value) => {
+        if (callback) callback(value);
+      }).catch((err) => {
+        console.error(err);
+      });
+  }
 
   /**
    * Loads the locale file of the given language.
@@ -212,24 +264,30 @@ export default class Si18n {
    * the locale is loaded.
    * @private
    */
-  #translate(lang = this.#options.lang) {
-    // Cannot translate to a language that doesn't exist.
-    if (lang && !Si18n.#isUndefined(this.#options.locales[lang])) {
-      this.#options.lang = lang;
+  #loadLocale({ lang, cb }) {
+    if (this.getLocales().includes(lang)) {
+      if (typeof cb === "function") cb();
     } else {
-      // Set fallback language.
-      this.#options.lang = this.#options.fallbackLang;
+      Si18n.getJSON(`${this.#options.path}/${lang}.json`, (locale) => {
+        this.#options.locales[lang] = locale;
+        if (typeof cb === "function") cb(locale);
+      });
     }
+  }
 
-    let _rtl = this.#options.locales[this.#options.lang].rtl;
   /**
    * Translates the page content by setting/replacing values to/of the elements
    * with the `data-i18n` attribute and call the `translate` callback
    * method option.
    * @private
    */
+  #translate() {
+    let _rtl = false;
+    if (!Si18n.#isUndefined(this.#options.locales[this.#options.lang]?.rtl))
+      _rtl = this.#options.locales[this.#options.lang].rtl;
+
+    document.documentElement.dir = _rtl ? "rtl" : "ltr";
     document.documentElement.lang = this.getLocale();
-    document.documentElement.dir = _rtl === "true" ? "rtl" : "ltr";
 
     // Auto translate using the given JSONPath in the data-si18n attribute.
     // When using this option, no need to write the script manually except
